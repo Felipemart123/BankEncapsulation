ad1b280adf8c99e348a63299fb58e5bcfe171414
Felipemart123 <felipemart1876@gmail.com>
2022-07-15 09:42:15 -0400

    Ex. 1

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace BankEncapsulation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var account = new BankAccount();
+
+        Console.WriteLine("How much do you want to deposit");
+        var amountToDeposit = double.Parse(Console.ReadLine());
+
+        account.Deposit(amountToDeposit);
+
+        Console.WriteLine($"Your balance is now { account.GetBalance()} ");
+    }
+}
